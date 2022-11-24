001b97e1bdb7f2596fd707f563b821ec55054365
Mehedi Hasan <mehedihasan2882@icloud.com>
2022-11-24 19:41:09 +0600

    Created the repo

---
+++ Miles to Kilometers.py
@@ -0,0 +1,16 @@
+# 1) Complete the function to return the result of the conversion
+def convert_distance(miles):
+	return miles * 1.6  # approximately 1.6 km in 1 mile
+
+my_trip_miles = int(input("Ensert the distance in Mile unit: \n"))
+
+# 2) Convert my_trip_miles to kilometers by calling the function above
+my_trip_km = convert_distance(my_trip_miles)
+
+# 3) Fill in the blank to print the result of the conversion
+print("The distance in kilometers is " + str(my_trip_km))
+
+# 4) Calculate the round-trip in kilometers by doubling the result,
+#    and fill in the blank to print the result
+round_trip=my_trip_km*2
+print("The round-trip in kilometers is " + str(round_trip))
